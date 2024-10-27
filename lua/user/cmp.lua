e7078433012b70b0506e3cfec0e5a55029a171a4
Prabhat Maurya <mauryaprabhat@tuta.io>
2024-10-27 11:57:14 +0530

    27 oct update

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -1,27 +1,48 @@
 -- ~/.config/nvim/lua/user/cmp.lua
 
-local cmp = require('cmp')
+local cmp = require("cmp")
 
-cmp.setup {
-  snippet = {
-    expand = function(args)
-      require('luasnip').lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
-  }),
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-  }, {
-    { name = 'buffer' },
-  })
-}
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			require("luasnip").lsp_expand(args.body)
+		end,
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Confirm selection with Enter
+
+		-- Tab to navigate forward through completion menu
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif require("luasnip").expand_or_jumpable() then
+				require("luasnip").expand_or_jump()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+
+		-- Shift-Tab to navigate backward through completion menu
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif require("luasnip").jumpable(-1) then
+				require("luasnip").jump(-1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+	}),
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" }, -- Snippet support
+	}, {
+		{ name = "buffer" }, -- Buffer as a fallback source
+	}),
+})
 
 -- Ensure that capabilities are extended for LSP
-local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
-
+local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
--- lua/user/test.lua
+++ lua/user/test.lua
@@ -1,10 +1,10 @@
 -- lua/user/test.lua
 return function()
-    -- Set up vim-test plugin
-    require('packer').use {
-        'vim-test/vim-test',
-        config = function()
-            vim.cmd [[
+	-- Set up vim-test plugin
+	require("packer").use({
+		"vim-test/vim-test",
+		config = function()
+			vim.cmd([[
                 let test#strategy = "neovim"
                 " Customize the keybindings or other settings here
                 nmap <silent> t<C-n> :TestNearest<CR>
@@ -12,8 +12,7 @@ return function()
                 nmap <silent> t<C-s> :TestSuite<CR>
                 nmap <silent> t<C-l> :TestLast<CR>
                 nmap <silent> t<C-g> :TestVisit<CR>
-            ]]
-        end
-    }
+            ]])
+		end,
+	})
 end
-
